215a396d581577b991c3c8e80d48f3fa01b681ce
leon-zheng <misterdayang@gmail.com>
2017-11-24 11:13:45 +0800

    little change

--- src/network2_trainer.py
+++ src/network2_trainer.py
@@ -9,5 +9,8 @@ epochs, mini_batch_size, eta, lamda, momenta = input("Please set the \
 iteration times, mini-batch size, learning rate, lamda and momentum: ")
 # net.large_weight_initializer()
 print "Well done, start training..."
-net.SGD(training_data, epochs, mini_batch_size, eta, evaluation_data=test_data,lmbda=lamda, momentum=momenta, monitor_evaluation_cost=True, monitor_evaluation_accuracy=True, monitor_training_cost=True,  monitor_training_accuracy=True)
+net.SGD(training_data, epochs, mini_batch_size, eta,
+    evaluation_data=test_data,lmbda=lamda, momentum=momenta,
+    monitor_evaluation_cost=True, monitor_evaluation_accuracy=True,
+    monitor_training_cost=True,  monitor_training_accuracy=True)
 print "Complete!"
